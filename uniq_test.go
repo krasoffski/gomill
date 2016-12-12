5994f8cce3664a5085d0c73ac059da48c2dcb9b6
Yury Krasouski <krasoffski@gmail.com>
2016-12-12 21:42:58 +0300

    Updated to use reflect in uniq test

--- uniq_test.go
+++ uniq_test.go
@@ -1,17 +1,15 @@
 package gomill
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestUniqInt(t *testing.T) {
 	in := []int{1, 2, 3, 4, 5, 2, 1, 3, -2, 4, 1, 99, 12, 2, 12, 3, 3}
 	exp := []int{1, 2, 3, 4, 5, -2, 99, 12}
 	res := UniqInt(in)
-	if len(exp) != len(res) {
-		t.Fail()
-	}
-	for i := range exp {
-		if exp[i] != res[i] {
-			t.Fail()
-		}
+	if !reflect.DeepEqual(exp, res) {
+		t.Errorf("expected slice %v is not equal to %v", exp, res)
 	}
 }
