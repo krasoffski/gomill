818d9a09044a8f2bdb4cc8d97715d37e2c0cc869
Yury Krasouski <yury_krasouski@epam.com>
2016-12-12 16:53:51 +0300

    Added uniq int func.

---
+++ uniq.go
@@ -0,0 +1,15 @@
+package gomill
+
+func UniqInt(s []int) []int {
+	lst := make([]int, 0, 0)
+	set := make(map[int]Empty)
+	for _, i := range s {
+		_, ok := set[i]
+		if ok {
+			continue
+		}
+		set[i] = Empty{}
+		lst = append(lst, i)
+	}
+    return lst
+}
