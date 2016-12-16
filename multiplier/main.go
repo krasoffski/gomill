6329d366dc99d70ce0c7833ac690a3d7a7745040
Yury Krasouski <krasoffski@gmail.com>
2016-12-16 21:35:32 +0300

    Simple cli flag.

--- multiplier/main.go
+++ multiplier/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"sync"
 )
 
 func main() {
 	var wg sync.WaitGroup
+
+	factor := flag.Int("factor", 0, "multiplier for enlarger")
+	flag.Parse()
+
 	ctx := NewContext()
 
 	counter := NewCounter(ctx, &wg)
 	c := counter.GetSource()
 
-	enlarger := NewEnlarger(ctx, &wg, c, 2)
+	enlarger := NewEnlarger(ctx, &wg, c, *factor)
 	r := enlarger.GetSource()
 
 	fmt.Println(<-r)
