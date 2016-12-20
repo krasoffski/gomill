2cbd819b353e8a6cde818ffb4e98dc5239eae44d
Yury Krasouski <krasoffski@gmail.com>
2016-12-20 22:08:15 +0300

    Fix adding Item to cache.

--- lru1/lru1.go
+++ lru1/lru1.go
@@ -1,18 +1,22 @@
+// Package lru1 implement Least Recently Used based on doubly linked list.
 package lru1
 
 import "container/list"
 
+// Item is an element in cache.
 type Item struct {
 	key   string
 	value string
 }
 
+// Cache is a sized LRU cache.
 type Cache struct {
 	capacity int
 	data     map[string]*list.Element
 	lst      *list.List
 }
 
+// NewCache returns an initialized LRU cache.
 func NewCache(capacity int) *Cache {
 	cache := new(Cache)
 	cache.capacity = capacity
@@ -20,13 +24,18 @@ func NewCache(capacity int) *Cache {
 	return cache
 }
 
+// Put inserts new Item to cache.
+// If cache is full removes oldest Item first.
 func (c *Cache) Put(key, value string) {
 	if len(c.data) == c.capacity {
 		delete(c.data, c.lst.Back().Value.(*Item).key)
 		c.lst.Remove(c.lst.Back())
 	}
+	c.data[key] = c.lst.PushFront(&Item{key, value})
 }
 
+// Get returns Item from cache by key.
+// nil is returned if there is no such key in the cache.
 func (c *Cache) Get(key string) *Item {
 	if c.data[key] != nil {
 		c.lst.MoveToFront(c.data[key])
