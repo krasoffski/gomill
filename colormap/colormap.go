895f8f52420ce15092d42ac49ee4f472bd4315ef
Yury Krasouski <krasoffski@gmail.com>
2017-01-09 17:43:06 +0300

    Funcs for HexStr colort representation.

--- colormap/colormap.go
+++ colormap/colormap.go
@@ -2,25 +2,30 @@
 // With givin value, value minimum and value maximum (given range) returns
 // RGB color from Blue for minimum value to Red for maximum value.
 // Intermediate colors such as green, yellow and more might also be returned for
-// values between minimum and maximum.
+// values between minimum and maximum (within range).
 package colormap
+
+import "fmt"
 
 type Range struct {
 	VMin, VMax float64
 }
 
-func (r *Range) Scale(val float64) (float64, float64, float64) {
-	return Scale(val, r.VMin, r.VMax)
+// Correlate returns red, green, blue values represented as float64 in range [0..1].
+func (r *Range) Correlate(val float64) (float64, float64, float64) {
+	return Correlate(val, r.VMin, r.VMax)
 }
 
-// Scale returns red, green, blue values represented as float64 in range [0..1].
-func Scale(val, vmin, vmax float64) (float64, float64, float64) {
-	var (
-		vrange float64
+func (r *Range) HexStr(val float64) string {
+	return HexStr(val, r.VMin, r.VMax)
+}
 
-		red   float64 = 1
-		green float64 = 1
-		blue  float64 = 1
+// Correlate returns red, green, blue values represented as float64 in range [0..1].
+func Correlate(val, vmin, vmax float64) (float64, float64, float64) {
+
+	var (
+		vrange  float64 = vmax - vmin
+		r, g, b float64 = 1, 1, 1
 	)
 
 	if val < vmin {
@@ -29,28 +34,27 @@ func Scale(val, vmin, vmax float64) (float64, float64, float64) {
 	if val > vmax {
 		val = vmax
 	}
-	vrange = vmax - vmin
 
 	if val < (vmin + 0.25*vrange) {
-		red = 0
-		green = 4 * (val - vmin) / vrange
+		r = 0
+		g = 4 * (val - vmin) / vrange
 	} else if val < (vmin + 0.5*vrange) {
-		red = 0
-		blue = 1 + 4*(vmin+0.25*vrange)/vrange
+		r = 0
+		b = 1 + 4*(vmin+0.25*vrange)/vrange
 	} else if val < (vmin+0.75*vrange)/vrange {
-		blue = 0
-		red = 4 * (val - vmin - 0.5*vrange) / vrange
+		b = 0
+		r = 4 * (val - vmin - 0.5*vrange) / vrange
 	} else {
-		blue = 0
-		green = 1 + 4*(vmin+0.75*vrange-val)/vrange
+		b = 0
+		g = 1 + 4*(vmin+0.75*vrange-val)/vrange
 	}
-	return red, green, blue
+	return r, g, b
 }
 
-// TODO: Think about func for uint8 returns.
+// TODO: Think about func which returns three uint8 values.
 
-// func HexStr(val, vmin, vmax float64) string {
-// 	var m float64 = 255
-// 	r, g, b := Scale(val, vmin, vmax)
-// 	return fmt.Sprintf("%X%X%X", strconv.uint8(r * m), uint8(g * m), uint8(b * m), uint8(m)}
-// }
+func HexStr(val, vmin, vmax float64) string {
+	const m = 0xFF
+	r, g, b := Correlate(val, vmin, vmax)
+	return fmt.Sprintf("%X%X%X", uint8(r*m), uint8(g*m), uint8(b*m))
+}
