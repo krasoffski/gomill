987ab24d9b6f8284433b6caa2c97c680c9e19443
Yury Krasouski <krasoffski@gmail.com>
2017-01-10 22:12:37 +0300

    Walking around names for htcmap functions.

--- htcmap/htcmap.go
+++ htcmap/htcmap.go
@@ -1,9 +1,9 @@
-// Package colormap implements a basic color mapping library.
+// Package htcmap implements a basic hot to color ramp functions.
 // With givin value, value minimum and value maximum (given range) returns
 // RGB color from Blue for minimum value to Red for maximum value.
 // Intermediate colors such as green, yellow and more might also be returned for
 // values between minimum and maximum (within range).
-package colormap
+package htcmap
 
 import "fmt"
 
@@ -11,19 +11,39 @@ type Range struct {
 	VMin, VMax float64
 }
 
-// Correlate returns red, green, blue values represented as float64 in range [0..1].
-func (r *Range) Correlate(val float64) (float64, float64, float64) {
-	return Correlate(val, r.VMin, r.VMax)
+func (r *Range) AsFloat(val float64) (float64, float64, float64) {
+	return AsFloat(val, r.VMin, r.VMax)
 }
 
-// HexStr returns red, green, blue values represented as six hex digits string
-// which begins with hash (#).
-func (r *Range) HexStr(val float64) string {
-	return HexStr(val, r.VMin, r.VMax)
+func (r *Range) AsUInt8(val float64) (uint8, uint8, uint8) {
+	return AsUInt8(val, r.VMin, r.VMax)
 }
 
-// Correlate returns red, green, blue values represented as float64 in range [0..1].
-func Correlate(val, vmin, vmax float64) (float64, float64, float64) {
+func (r *Range) AsStr(val float64) string {
+	return AsStr(val, r.VMin, r.VMax)
+}
+
+// AsFloat returns red, green, blue values each as float64 in range [0..1].
+func AsFloat(val, vmin, vmax float64) (float64, float64, float64) {
+	return htc(val, vmin, vmax)
+}
+
+// AsUInt8 returns red, green, blue values each as uint8 in range [0..255].
+func AsUInt8(val, vmin, vmax float64) (uint8, uint8, uint8) {
+	const m = 0xFF
+	r, g, b := AsFloat(val, vmin, vmax)
+	return uint8(r * m), uint8(g * m), uint8(b * m)
+}
+
+// AsStr returns red, green, blue values represented as six hex digits string
+// which begins with hash (#). It looks like a HTML RGB color #00FF00.
+func AsStr(val, vmin, vmax float64) string {
+	r, g, b := AsUInt8(val, vmin, vmax)
+	return fmt.Sprintf("#%02X%02X%02X", r, g, b)
+}
+
+// Stop points can be modified/adjusted for better correlation.
+func htc(val, vmin, vmax float64) (float64, float64, float64) {
 
 	var (
 		vrange  float64 = vmax - vmin
@@ -51,13 +71,4 @@ func Correlate(val, vmin, vmax float64) (float64, float64, float64) {
 		g = 1 + 4*(vmin+0.75*vrange-val)/vrange
 	}
 	return r, g, b
-}
-
-// HexStr returns red, green, blue values represented as six hex digits string
-// which begins with hash (#).
-func HexStr(val, vmin, vmax float64) string {
-	const m = 0xFF
-	r, g, b := Correlate(val, vmin, vmax)
-	// TODO: Think about func which returns three uint8 values.
-	return fmt.Sprintf("#%02X%02X%02X", uint8(r*m), uint8(g*m), uint8(b*m))
 }
