23859162a2fd44539e31918fa747166723918dce
Yury Krasouski <krasoffski@gmail.com>
2017-04-07 13:20:15 +0300

    Updated package doc for memosort.

--- memosort/memosort.go
+++ memosort/memosort.go
@@ -1,30 +1,19 @@
 // Package memosort implements a function which keeps order from previous sort
 // for equal elements. It behaves as sort.Stable but only requires less function
-// as argument and can be used with sort.Slice. Here is short example:
-// m := memosort.New()
-// sort.Slice(tracks, m.By(func(i, j int) bool {
-// 	return tracks[i].Title < tracks[j].Title
-// }))
-// sort.Slice(tracks, m.By(func(i, j int) bool {
-// 	return tracks[i].Year < tracks[j].Year
-// }))
-// sort.Slice(tracks, m.By(func(i, j int) bool {
-// 	return tracks[i].Length < tracks[j].Length
-// }))
-
+// as argument and can be used with sort.Slice.
 package memosort
 
-// MemoSort holds sort function call order.
+// MemoSort holds sort functions call order.
 type MemoSort []func(i, j int) bool
 
-// By appends less function to internal storage and returns new one which keeps
+// By appends less function to slice and returns new less function which keeps
 // order from previous sort for equal elements.
 func (m *MemoSort) By(f func(i, j int) bool) func(i, j int) bool {
 	*m = append(*m, f)
 	return m.Less
 }
 
-// Less implemented interface order from previous sort for equal elements.
+// Less compares two elements using all previous less functions calls.
 func (m *MemoSort) Less(i, j int) bool {
 	for _, fn := range *m {
 		switch {
