74534191d129ce727e81b1d1c22db10325ef0999
Yury Krasouski <krasoffski@gmail.com>
2017-09-08 12:51:01 +0300

    add simple print test for memosort

--- memosort/memosort_test.go
+++ memosort/memosort_test.go
@@ -1,7 +1,11 @@
 package memosort
 
 import (
+	"fmt"
+	"os"
+	"sort"
 	"testing"
+	"text/tabwriter"
 	"time"
 )
 
@@ -13,18 +17,19 @@ type Track struct {
 	Length time.Duration
 }
 
-var tracks = []*Track{
+var tracks = []Track{
 	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
 	{"Go", "Moby", "Moby", 1992, length("3m37s")},
 	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
 	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
-	{"Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975, length("6m6s")},
-	{"Smells Like Teen Spirit", "Nirvana", "Nevermind", 1972, length("4m37s")},
-	{"Imagine", "John Lennon", "Imagine", 1971, length("3m54s")},
-	{"Hotel California", "Eagles", "Hotel California", 1976, length("6m40s")},
-	{"One", "Metallica", "And Justice for All", 1988, length("7m23s")},
-	{"Comfortably Numb", "Pink Floyd", "The Wall", 1979, length("6m53s")},
-	{"Like a Rolling Stone", "Bob Dylan", "Highway 61 Revisited", 1965, length("6m20s")},
+
+	// {"Comfortably Numb", "Pink Floyd", "The Wall", 1979, length("6m53s")},
+	// {"Like a Rolling Stone", "Bob Dylan", "Highway 61 Revisited", 1965, length("6m20s")},
+	// {"Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975, length("6m6s")},
+	// {"Smells Like Teen Spirit", "Nirvana", "Nevermind", 1972, length("4m37s")},
+	// {"Imagine", "John Lennon", "Imagine", 1971, length("3m54s")},
+	// {"Hotel California", "Eagles", "Hotel California", 1976, length("6m40s")},
+	// {"One", "Metallica", "And Justice for All", 1988, length("7m23s")},
 }
 
 func length(s string) time.Duration {
@@ -35,6 +40,28 @@ func length(s string) time.Duration {
 	return d
 }
 
-func TestStableSimple(t *testing.T) {
+func printTracks(tracks []Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	fmt.Println()
+	tw.Flush()
+}
 
+func TestStableSimple(t *testing.T) {
+	tracksMemo := make([]Track, len(tracks))
+	copy(tracksMemo, tracks)
+
+	printTracks(tracksMemo)
+
+	m := New()
+	m.By(func(i, j int) bool { return tracksMemo[i].Title < tracksMemo[j].Title })
+	m.By(func(i, j int) bool { return tracksMemo[i].Year < tracksMemo[j].Year })
+	m.By(func(i, j int) bool { return tracksMemo[i].Length < tracksMemo[j].Length })
+	sort.Slice(tracksMemo, m.Less)
+	printTracks(tracksMemo)
 }
