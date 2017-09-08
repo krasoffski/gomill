ea9dc62f307a2175c3ee3a3f13d78a7b79df69c2
Yury Krasouski <krasoffski@gmail.com>
2017-09-08 13:53:02 +0300

    finish  small test for memosort

--- memosort/memosort.go
+++ memosort/memosort.go
@@ -1,5 +1,7 @@
 // Package memosort implements a multi-tier ordering function Less with ability
 // to specify primary, secondary and so forth comparing functions.
+// Adding orders plays key role for comparison. Like first added function is
+// primary comparing function, secondary added functin is secondary key.
 // This functionality should be used with sort.Slice. Please, take a look on
 // testing file for usage examples.
 package memosort
@@ -9,8 +11,9 @@ type MemoSort []func(i, j int) bool
 
 // By appends less function to slice and returns new less function which keeps
 // order from previous sort for equal elements.
-func (m *MemoSort) By(f func(i, j int) bool) func(i, j int) bool {
-	*m = append(*m, f)
+// Don't define new type for less function for better documentation.
+func (m *MemoSort) By(f ...func(i, j int) bool) func(i, j int) bool {
+	*m = append(*m, f...)
 	return m.Less
 }
 
--- memosort/memosort_test.go
+++ memosort/memosort_test.go
@@ -1,11 +1,8 @@
 package memosort
 
 import (
-	"fmt"
-	"os"
 	"sort"
 	"testing"
-	"text/tabwriter"
 	"time"
 )
 
@@ -22,14 +19,13 @@ var tracks = []Track{
 	{"Go", "Moby", "Moby", 1992, length("3m37s")},
 	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
 	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
-
-	// {"Comfortably Numb", "Pink Floyd", "The Wall", 1979, length("6m53s")},
-	// {"Like a Rolling Stone", "Bob Dylan", "Highway 61 Revisited", 1965, length("6m20s")},
-	// {"Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975, length("6m6s")},
-	// {"Smells Like Teen Spirit", "Nirvana", "Nevermind", 1972, length("4m37s")},
-	// {"Imagine", "John Lennon", "Imagine", 1971, length("3m54s")},
-	// {"Hotel California", "Eagles", "Hotel California", 1976, length("6m40s")},
-	// {"One", "Metallica", "And Justice for All", 1988, length("7m23s")},
+	{"Comfortably Numb", "Pink Floyd", "The Wall", 1979, length("6m53s")},
+	{"Like a Rolling Stone", "Bob Dylan", "Highway 61 Revisited", 1965, length("6m20s")},
+	{"Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975, length("6m6s")},
+	{"Smells Like Teen Spirit", "Nirvana", "Nevermind", 1972, length("4m37s")},
+	{"Imagine", "John Lennon", "Imagine", 1971, length("3m54s")},
+	{"Hotel California", "Eagles", "Hotel California", 1976, length("6m40s")},
+	{"One", "Metallica", "And Justice for All", 1988, length("7m23s")},
 }
 
 func length(s string) time.Duration {
@@ -40,28 +36,44 @@ func length(s string) time.Duration {
 	return d
 }
 
-func printTracks(tracks []Track) {
-	const format = "%v\t%v\t%v\t%v\t%v\t\n"
-	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
-	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
-	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
-	for _, t := range tracks {
-		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+func TestMemoSimple(t *testing.T) {
+	te := make([]Track, len(tracks)) // tracks expected
+	tm := make([]Track, len(tracks)) // tracks memosort
+	copy(te, tracks)
+	copy(tm, tracks)
+
+	ms := New() // memo sort
+	ms.By(func(i, j int) bool { return tm[i].Title < tm[j].Title })
+	ms.By(func(i, j int) bool { return tm[i].Year < tm[j].Year })
+	ms.By(
+		func(i, j int) bool { return tm[i].Length < tm[j].Length },
+		func(i, j int) bool { return tm[i].Artist < tm[j].Artist },
+		func(i, j int) bool { return tm[i].Album < tm[j].Album },
+	)
+	sort.Slice(tm, ms.Less)
+
+	sort.Slice(te, func(i, j int) bool {
+		if te[i].Title != te[j].Title {
+			return te[i].Title < te[j].Title
+		}
+		if te[i].Year != te[j].Year {
+			return te[i].Year < te[j].Year
+		}
+		if te[i].Length != te[j].Length {
+			return te[i].Length < te[j].Length
+		}
+		if te[i].Artist != te[j].Artist {
+			return te[i].Artist < te[j].Artist
+		}
+		if te[i].Album != te[j].Album {
+			return te[i].Album < te[j].Album
+		}
+		return false
+	})
+
+	for i, track := range te {
+		if track != tm[i] {
+			t.Errorf("i: %v, expected %v, got %v", i, track, tm[i])
+		}
 	}
-	fmt.Println()
-	tw.Flush()
-}
-
-func TestStableSimple(t *testing.T) {
-	tracksMemo := make([]Track, len(tracks))
-	copy(tracksMemo, tracks)
-
-	printTracks(tracksMemo)
-
-	m := New()
-	m.By(func(i, j int) bool { return tracksMemo[i].Title < tracksMemo[j].Title })
-	m.By(func(i, j int) bool { return tracksMemo[i].Year < tracksMemo[j].Year })
-	m.By(func(i, j int) bool { return tracksMemo[i].Length < tracksMemo[j].Length })
-	sort.Slice(tracksMemo, m.Less)
-	printTracks(tracksMemo)
 }
