06fd253c1ff501dfb2b29ec6b6e90a199f34459d
Yury Krasouski <krasoffski@gmail.com>
2017-10-16 20:13:30 +0300

    removed moztop module

--- crawler/cmd/crawler/main.go
+++ crawler/cmd/crawler/main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/krasoffski/gomill/crawler"
 )
+
+// mozTop creates new reader which represents list of sites addresses from
+// https://moz.com/top500 delimeted by new line charaster.
+// Request timeout is 60s.
+func mozTop(head int) io.Reader {
+	// TODO: Create a better solution for providing top sites.
+
+	client := http.Client{Timeout: time.Second * 60}
+	fmt.Printf("Getting csv with top 500 from moz.com... ")
+	start := time.Now()
+	resp, err := client.Get("https://moz.com/top500/domains/csv")
+
+	if err != nil {
+		log.Fatalf("error performing request %s\n", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("error getting moz top 500: %s\n", resp.Status)
+	}
+	if !strings.Contains(resp.Header.Get("Content-Type"), "text/csv") {
+		log.Fatalln("error getting csv data, wrong conten type")
+	}
+	defer resp.Body.Close()
+
+	urls := make([]string, head)
+	reader := csv.NewReader(resp.Body)
+
+	for i := 0; i <= head; i++ {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error reading csv %s\n", err)
+		}
+		url := "http://" + strings.TrimRight(record[1], "/")
+
+		urls = append(urls, url)
+	}
+
+	// Do not count strings join and new reader creation.
+	fmt.Printf("done in %.2fs\n", time.Since(start).Seconds())
+	// Skipping csv header.
+	return strings.NewReader(strings.Join(urls[1:], "\n"))
+}
 
 func main() {
 	workers := flag.Int("workers", 2, "number of workers (goroutines)")
@@ -24,7 +71,7 @@ func main() {
 	var reader io.Reader = os.Stdin
 	if *topsite > 0 {
 		if *topsite < 500 {
-			reader = crawler.MozReader(*topsite)
+			reader = mozTop(*topsite)
 		} else {
 			fmt.Fprintf(os.Stderr,
 				"error value: %d, specify top sites within range [1, 500]\n",
