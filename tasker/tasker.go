517c5628a527bb5292dd9f47ca5e4c2dbda79db3
Yury Krasouski <krasoffski@gmail.com>
2017-10-16 20:30:52 +0300

    doc fixes for tasker

--- tasker/tasker.go
+++ tasker/tasker.go
@@ -1,24 +1,28 @@
+// Package tasker provide high level interfaces for creating asynchronous
+// task creation and execution.
 package tasker
 
 import (
 	"sync"
 )
 
-// Task describes
+// Task describes minimal interface for simple tasks.
 type Task interface {
 	Process()
 	Output()
 }
 
-// Manufacturer describes
-type Manufacturer interface {
+// Builder describes interface for creating tasks from slice of strings.
+type Builder interface {
 	BufSize() int
 	Create(line string) Task
 	Items() <-chan string
 }
 
-// Run executes
-func Run(m Manufacturer, workers int) {
+// Run creates tasks from slice of strings and performs this task.
+// After task completion method Output is invoked for each tasks.
+// Argument workers respresents number of goroutines for performing tasks.
+func Run(m Builder, workers int) {
 	var wg sync.WaitGroup
 	in := make(chan Task, m.BufSize())
 
