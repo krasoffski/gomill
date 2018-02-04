5ee920db1a3f2add660385d312e1b5ff4573ca62
Yury Krasouski <krasoffski@gmail.com>
2018-02-04 14:42:02 +0300

    add AnyFnN and AllFnN

--- node/checks.go
+++ node/checks.go
@@ -4,24 +4,43 @@ import "golang.org/x/net/html"
 
 // AllFn ...
 func AllFn(funcs ...func(*html.Node) bool) func(*html.Node) bool {
-	return func(n *html.Node) bool {
-		for _, fn := range funcs {
-			if !fn(n) {
-				return false
-			}
-		}
-		return true
-	}
+	return all(true, funcs...)
+}
+
+// AllFnN ...
+func AllFnN(funcs ...func(*html.Node) bool) func(*html.Node) bool {
+	return all(false, funcs...)
 }
 
 // AnyFn ...
 func AnyFn(funcs ...func(*html.Node) bool) func(*html.Node) bool {
+	return any(true, funcs...)
+}
+
+// AnyFnN ...
+func AnyFnN(funcs ...func(*html.Node) bool) func(*html.Node) bool {
+	return any(false, funcs...)
+}
+
+// AllFn ...
+func all(okRes bool, funcs ...func(*html.Node) bool) func(*html.Node) bool {
+	return func(n *html.Node) bool {
+		for _, fn := range funcs {
+			if !fn(n) {
+				return !okRes
+			}
+		}
+		return okRes
+	}
+}
+
+func any(okRes bool, funcs ...func(*html.Node) bool) func(*html.Node) bool {
 	return func(n *html.Node) bool {
 		for _, fn := range funcs {
 			if fn(n) {
-				return true
+				return okRes
 			}
 		}
-		return false
+		return !okRes
 	}
 }
